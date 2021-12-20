22ea659bc5ba43beafd952f6e5245bac42684255
Philipp Hagedorn <77963403+philhag@users.noreply.github.com>
2021-12-20 13:40:49 +0100

    Update dependencies and imports

--- src/main/java/org/iib/icdd2ams/ToSQLTemplate.java
+++ src/main/java/org/iib/icdd2ams/ToSQLTemplate.java
@@ -6,9 +6,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
